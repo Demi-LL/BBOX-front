e8872eb6ad00137bae33651b5f115a47ceef7ff5
Demi-LL <liuliunft@gmail.com>
2022-03-05 09:57:39 +0800

    feat: 初始化專案
    1. 使用 NUXT.js
    2. 使用 bootstrap

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-8">
+        blablablablablablablablablablablablablablablablablablablablabla
+        blablablablablablablablablablablablablablablablablablablablabla
+        blablablablablablablablablablablablablablablablablablablablabla
+        blablablablablablablablablablablablablablablablablablablablabla
+        blablablablablablablablablablablablablablablablablablablablabla
+        blablablablablablablablablablablablablablablablablablablablabla
+      </div>
+      <div class="col-4">
+        dodododoblablablablablablablablablablablablablablablablablablablablabla
+        blablablablablablablablablablablablablablablablablablablablabla
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
